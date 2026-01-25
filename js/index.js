fe82c0d02521f0d1f6947d7f8bc721ee2633a35f
Serhii0080 <startap.chrom@gmail.com>
2026-01-25 05:12:06 +0200

    add-favorites

--- js/index.js
+++ js/index.js
@@ -5,16 +5,47 @@ const model = {
         const note = {};
         note.title = inputValue;
         note.description = textareaValue;
-        note.id = new Date().getTime();
+        note.id = crypto.randomUUID();
         note.color = colorInput;
         note.isFavorite = false;
-        this.notes.push(note);
+        this.notes.unshift(note);
         view.renderNotes(this.notes);
+        this.updateNotesLength();
+    },
+
+    updateNotesLength() {
+        const notesLength = this.notes.length;
+        view.updateNotesCount(notesLength);
+        view.updateNotesMessage(notesLength);
+    },
+
+    removeNote(id) {
+        const index = this.notes.findIndex((element) => element.id === id);
+        if (index !== -1) {
+            this.notes.splice(index, 1);
+        }
+        view.renderNotes(this.notes);
+        this.updateNotesLength();
+    },
+
+    toggleFavorite(noteId, newFavoriteState) {
+        this.notes.forEach((element) => {
+            if (element.id === noteId) {
+                element.isFavorite = newFavoriteState;
+            }
+        });
+    },
+
+    getFavoriteNotes() {
+        return this.notes.filter((note) => note.isFavorite);
     },
 };
 
 const view = {
     init() {
+        this.deleteNote();
+        this.notesFavorite();
+        this.setupFavoriteButton();
         const form = document.querySelector(".note-form");
         const button = document.querySelector(".main-button");
         const input = document.querySelector(".name-noties");
@@ -25,7 +56,7 @@ const view = {
             const inputValue = input.value;
             const textareaValue = textarea.value;
             const checked = document.querySelector(
-                'input[name="color"]:checked'
+                'input[name="color"]:checked',
             );
             let colorInput;
 
@@ -51,38 +82,128 @@ const view = {
                         // Здесь можно задать цвет по умолчанию, если понадобится!
                         break;
                 }
+                input.value = "";
+                textarea.value = "";
 
                 controller.addNote(inputValue, textareaValue, colorInput);
             }
         });
     },
 
-    renderNotes(noties) {
+    renderNotes(notes) {
+        this.updateFavoriteText();
         const notesList = document.querySelector(".notes-list");
-        const filterBox = document.querySelector(".filter-box");
         notesList.innerHTML = "";
-        filterBox.innerHTML = "";
-        filterBox.style.marginTop = "0";
-        filterBox.innerHTML = `<label class="filterCheckbox">
-    <input type="checkbox" />Показать только избранные заметки
-</label>`;
 
-        noties.forEach((element) => {
+        const showOnlyFavorites = document.querySelector(".active")
+            ? true
+            : false;
+
+        notes.forEach((element) => {
+            if (showOnlyFavorites && !element.isFavorite) return;
+
             const li = document.createElement("li");
             li.classList.add("addList");
+            li.id = element.id;
             li.innerHTML = `
-                <header class="headerNot">
-                <h1>
-                ${element.title}
-                <span class="group">
-                    <span class="favorite-false"></span>
-                    <span class="delete-false"></span>
-                </span>
-                </h1>
-                </header>
-                <p>${element.description}</p>`;
+            <header class="headerNot">
+            <h2>
+            ${element.title}
+            <span class="group">
+                <span class="noteFavorite" role="checkbox" aria-pressed="false"></span>
+                <span class="isDeleted"></span>
+            </span>
+            </h2>
+            </header>
+            <p>${element.description}</p>`;
             li.firstElementChild.classList.add(element.color);
+
+            if (element.isFavorite) {
+                const favoriteBtn = li.querySelector(".noteFavorite");
+                favoriteBtn.classList.add("selected");
+                favoriteBtn.setAttribute("aria-pressed", "true");
+            }
+
             notesList.append(li);
+        });
+    },
+
+    updateFavoriteText() {
+        const filterBox = document.querySelector(".filter-box");
+
+        if (!filterBox.querySelector(".filter-wrapper")) {
+            filterBox.style.paddingTop = "24px";
+            filterBox.innerHTML = `
+        <div class="filter-wrapper">
+            <button class="filterCheckbox" type="button" role="checkbox" aria-pressed="false"></button>
+            <span class="filterText">Показать только избранные заметки</span>
+        </div>`;
+        }
+    },
+
+    updateNotesMessage(length) {
+        const filterBox = document.querySelector(".filter-box");
+        if (length === 0) {
+            filterBox.style.paddingTop = "140px";
+            filterBox.innerHTML = `
+            <p>У вас нет еще ни одной заметки</p>
+            <p>Заполните поля выше и создайте свою первую заметку!</p>
+        `;
+        }
+    },
+
+    updateNotesCount(length) {
+        const count = document.querySelector(".count");
+        count.textContent = length;
+    },
+
+    deleteNote() {
+        const notesList = document.querySelector(".notes-list");
+
+        notesList.addEventListener("click", (event) => {
+            if (!event.target.classList.contains("isDeleted")) return;
+            const li = event.target.closest("li");
+            if (!li) return;
+            controller.removeNote(li.id);
+        });
+    },
+
+    setupFavoriteButton() {
+        const filterBox = document.querySelector(".filter-box");
+
+        filterBox.addEventListener("click", (event) => {
+            const filterButton = event.target.closest(".filterCheckbox");
+            if (!filterButton) return;
+
+            const pressed =
+                filterButton.getAttribute("aria-pressed") === "true";
+            filterButton.setAttribute("aria-pressed", !pressed);
+            filterButton.classList.toggle("active");
+
+            if (!pressed) {
+                this.renderNotes(model.getFavoriteNotes());
+            } else {
+                this.renderNotes(model.notes);
+            }
+        });
+    },
+
+    notesFavorite() {
+        const notesList = document.querySelector(".notes-list");
+
+        notesList.addEventListener("click", (event) => {
+            const noteFavorite = event.target.closest(".noteFavorite");
+            if (!noteFavorite) return;
+
+            const li = noteFavorite.closest("li");
+            if (!li) return;
+
+            const pressed =
+                noteFavorite.getAttribute("aria-pressed") === "true";
+            noteFavorite.setAttribute("aria-pressed", !pressed);
+            noteFavorite.classList.toggle("selected");
+            const newFavoriteState = !pressed;
+            controller.updateNoteFavorite(li.id, newFavoriteState);
         });
     },
 };
@@ -90,6 +211,12 @@ const view = {
 const controller = {
     addNote(inputValue, textareaValue, colorInput) {
         model.createNotes(inputValue, textareaValue, colorInput);
+    },
+    removeNote(id) {
+        model.removeNote(id);
+    },
+    updateNoteFavorite(noteId, newFavoriteState) {
+        model.toggleFavorite(noteId, newFavoriteState);
     },
 };
 
